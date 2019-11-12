2de0f7f2de843b19215055ccf8c028411bf3755b
William Powell <williampowell@William-Powells-MacBook-Pro.local>
2019-11-11 22:57:41 -0800

    Got all scores of the previous ten year games. Need to do data cleaning . Add home and vistor team, winner, score, etc

---
+++ Data wrangling/all_year_schedules.py
@@ -0,0 +1,24 @@
+# %%
+from bs4 import BeautifulSoup
+import pandas
+import numpy
+import requests
+
+
+# %%
+
+years = range(2010, 2020)
+for x in years:
+    Url = f"https://www.masseyratings.com/scores.php?s=cf{x}&sub=ncaa-d1&all=1&sch=on"
+    url_sourcecode = requests.get(Url)
+    urlPlaneText = url_sourcecode.text
+    urlSoup = BeautifulSoup(urlPlaneText, "html.parser")
+    pre = urlSoup.find_all("pre")[0]
+    stringpre = pre.string
+    test = open(
+        "Data wrangling/data/all_years_schedule.txt", "a+")
+    test.write(stringpre)
+    test.close()
+
+
+# %%
